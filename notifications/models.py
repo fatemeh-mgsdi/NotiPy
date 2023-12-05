0d64572d40a820798764e0611da635e645e9aab2
fatemeh-mgsdi <fatemeh.maghsoudi76@gmail.com>
2023-12-05 14:49:06 +0330

    feat: add notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class EmailTemplate(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    subject = models.CharField(max_length=255)
+    template = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Notification(models.Model):
+    message = models.TextField(blank=True, null=True)
+    receiver = models.EmailField()
+    status = models.CharField(max_length=50, default='pending')
+    template = models.ForeignKey('EmailTemplate', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.topic
