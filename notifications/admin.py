6c15d6e4548416db5c8a0d2a38c57e79297f942b
fatemeh-mgsdi <fatemeh.maghsoudi76@gmail.com>
2023-12-05 14:47:28 +0330

    feat: add models to admin panel

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Notification, EmailTemplate
+
+admin.site.register(Notification)
+admin.site.register(EmailTemplate)
